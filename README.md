707189859cd2ac6f690dfa7936017de5db08934d
KJ <kj3641402@gmail.com>
2021-02-13 22:45:28 -0500

    Used the starter code this time

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # password-generator
+
+Provided with an HTML and a CSS file, I developed a secure password generator using Javascript.  When the user clicks the button on the page, prompts and confirm messages appear to determine password requirements such as password length and character type.  The input is validated to ensure the requirments of at least one character type and a length of 8-128 characters are met.  Once the prompts are answered, a randomly generated password that fits the criteria is displayed on the page as seen below.
+
+
+![Screenshot 1 of site](./assets/images/screenshot-1.png)
+
+
+The page has been deployed at [kellyjohnson364.github.io/password-generator](https://kellyjohnson364.github.io/password-generator/)
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,69 +1,77 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// Function started when button pushed
 
-let getStarted = function() {
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword() 
+  var passwordText = document.querySelector("#password");
 
-// Strings of possible character choices
-
-    let stringSpecial = ("!#$%&()*+-/:;<=>?@\[^_{|}~];")
-    let stringUpper = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    let stringLower = ("abcdefghijklmnopqrstuvwxyz");
-    let stringNumber = ("12345678901234567890123456");
-    
-// Collection of character choice from the user using confirm 
-
-    let inputUpper =confirm("Would you like uppercase characters? \n 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'\n(select 'cancel' if no)");
-    let inputLower =confirm("Would you like lowercase characters? \n 'abcdefghijklmnopqrstuvwxyz'\n(select 'cancel' if no)");   
-    let inputNumber =confirm("Would you like number characters? \n '1234567890'\n(select 'cancel' if no)");
-    let inputSpecial =confirm("Would you like special characters? \n '!#$%&()*+-/:;<=>?@\[^_{|}~];'\n(select 'cancel' if no)");
-
-    // Combining of all selected characters into one string  
-
-    let stringFull=""
-
-    if (inputUpper) {
-       stringFull += stringUpper;
-    }
-    if (inputLower) {
-        stringFull += stringLower;
-    }
-    if (inputNumber) {
-        stringFull += stringNumber;
-    }
-    if (inputSpecial) {
-        stringFull += stringSpecial;
-    }
-    if (!stringFull) {
-    alert("Please select at least one character type");
-    return;   
-    }
-// Collection of password length from user and validation of range
-    
-    let countInput = window.prompt("Number of characters (8-128)", "8");
-        if (countInput < 8 || countInput > 128) {
-            alert("Value should be between 8 - 128");
-            countInput = window.prompt("Number of characters (8-128)", "8");
-        }
-
-// Selection of password characters from full string using random selection.
-// For loop is used to generate characters that are added into the password string until desired length is acheived.
-    
-    let character 
-    let passWord= ''
-   
-
-    for (let i = 0;  i < countInput; ++i) {
-        index = Math.floor(Math.random() * stringFull.length);
-        character = stringFull[index];
-        passWord += character;
-    }
-
-// Password is presented in an alert box
-
-    document.getElementById("product").innerHTML = passWord;
-
-//Page is reset following the closing of the alert box to prevent accumulation of countInput data.    
-
- 
+  passwordText.value = password;
 
 }
+  // Strings of possible character choices
+  
+      let stringSpecial = ("!#$%&()*+-/:;<=>?@\[^_{|}~];")
+      let stringUpper = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+      let stringLower = ("abcdefghijklmnopqrstuvwxyz");
+      let stringNumber = ("12345678901234567890123456");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+
+      
+  // Collection of character choice from the user using confirm 
+  
+      let inputUpper =confirm("Would you like uppercase characters? \n 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'\n(select 'cancel' if no)");
+      let inputLower =confirm("Would you like lowercase characters? \n 'abcdefghijklmnopqrstuvwxyz'\n(select 'cancel' if no)");   
+      let inputNumber =confirm("Would you like number characters? \n '1234567890'\n(select 'cancel' if no)");
+      let inputSpecial =confirm("Would you like special characters? \n '!#$%&()*+-/:;<=>?@\[^_{|}~];'\n(select 'cancel' if no)");
+  
+      // Combining of all selected characters into one string  
+  
+      let stringFull=""
+  
+      if (inputUpper) {
+         stringFull += stringUpper;
+      }
+      if (inputLower) {
+          stringFull += stringLower;
+      }
+      if (inputNumber) {
+          stringFull += stringNumber;
+      }
+      if (inputSpecial) {
+          stringFull += stringSpecial;
+      }
+      if (!stringFull) {
+      alert("Please select at least one character type");
+      writePassword();
+      }
+  // Collection of password length from user and validation of range
+      
+      let countInput = window.prompt("Number of characters (8-128)", "8");
+          if (countInput < 8 || countInput > 128) {
+              alert("Value should be between 8 - 128");
+              countInput = window.prompt("Number of characters (8-128)", "8");
+          }
+  
+  // Selection of password characters from full string using random selection.
+  // For loop is used to generate characters that are added into the password string until desired length is acheived.
+      
+      let character 
+     let passWord= ''
+     
+  
+      for (let i = 0;  i < countInput; ++i) {
+          index = Math.floor(Math.random() * stringFull.length);
+          character = stringFull[index];
+          passWord += character;
+      }
+  
+
+
+   return passWord;
+    }
